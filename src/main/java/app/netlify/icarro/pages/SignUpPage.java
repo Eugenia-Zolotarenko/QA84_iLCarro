bee545653549269b50fbcc4b39845b4b04c2f7a8
Arkadii <qatiktak@gmail.com>
2026-09-21 20:41:50 +0200

    Add4 isPageTitleSignUpCorrectPositiveSmokeTest createAccountPositiveTest TestBase - +newEmail, +clickButton

--- src/main/java/app/netlify/icarro/pages/SignUpPage.java
+++ src/main/java/app/netlify/icarro/pages/SignUpPage.java
@@ -1,10 +1,79 @@
 package app.netlify.icarro.pages;
 
 import app.netlify.icarro.core.BasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class SignUpPage extends BasePage {
     public SignUpPage(WebDriver driver) {
         super(driver);
+    }
+
+    public SignUpPage getSignUpPage() {
+        WebElement link = driver.findElement(By.cssSelector("h1[class='title']"));
+        clickWithJS(link);
+        return new SignUpPage(driver);
+    }
+
+    @FindBy(css = "input[name='firstName']")
+    WebElement firstNameInput;
+
+    @FindBy(css = "input[name='lastName']")
+    WebElement lastNameInput;
+
+    @FindBy(css = "input[name='username']")
+    WebElement usernameInput;
+
+    @FindBy(css = "input[name='password']")
+    WebElement passwordInput;
+
+    @FindBy(id = "terms-of-use")
+    WebElement termsCheckbox;
+
+    @FindBy(css = "button.btn.btn--primary")
+    WebElement submitButton;
+
+    public void fillFirstName(String firstName) {
+        fillField(firstNameInput, firstName);
+    }
+
+    public void fillLastName(String lastName) {
+        fillField(lastNameInput, lastName);
+    }
+
+    public void fillEmail(String userEmail) {
+        fillField(usernameInput, userEmail);
+    }
+
+    public void fillPassword(String password) {
+        fillField(passwordInput, password);
+    }
+
+    public void acceptTerms() {
+        click(termsCheckbox);
+    }
+
+    public void clickSubmit() {
+        click(submitButton);
+    }
+
+    private void fillField(WebElement element, String text) {
+        getWait(10).until(ExpectedConditions.visibilityOf(element));
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public void clickModalOkButton(By okButtonLocator) {
+        WebElement okButton = getWait(10).until(
+                ExpectedConditions.elementToBeClickable(okButtonLocator)
+        );
+        okButton.click();
+    }
+
+    public boolean isLogOutButtonPresent() {
+        return isElementPresent(By.cssSelector("button[class='navigation-link linklike']"));
     }
 }
--- src/test/java/app/netlify/icarro/tests/SignUpPageTests.java
+++ src/test/java/app/netlify/icarro/tests/SignUpPageTests.java
@@ -1,7 +1,56 @@
 package app.netlify.icarro.tests;
 
 import app.netlify.icarro.core.TestBase;
+import app.netlify.icarro.pages.SignUpPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import java.time.Duration;
+
 
 public class SignUpPageTests extends TestBase {
+
+    SignUpPage signUp;
+
+    @BeforeMethod
+    public void preconditions() {
+        //driver.navigate().to(URLHOMEPAGE);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement signUpLink = wait.until(
+                ExpectedConditions.elementToBeClickable(
+                        By.cssSelector("a.navigation-link[href='/register']")));
+        signUpLink.click();
+
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.tagName("h1"))
+        );
+        signUp = new SignUpPage(driver).getSignUpPage();
+    }
+
+
+    @Test(groups = {"smoke"})
+    public void isPageTitleSignUpCorrectPositiveSmokeTest() {
+        signUp.isPageTitleCorrect("Registration");
+    }
+
+    @Test(groups = {"regr"})
+    public void createAccountPositiveTest() {
+        SoftAssert softly = new SoftAssert();
+        signUp.fillFirstName("Sara");
+        signUp.fillLastName("Barabu");
+        signUp.fillEmail(newEmail());
+        signUp.fillPassword("Ss1a2r3a!");
+        signUp.acceptTerms();
+        signUp.clickSubmit();
+        signUp.clickModalOkButton(By.xpath("//button[.='OK']"));
+
+        softly.assertTrue(
+                signUp.isLogOutButtonPresent(),
+                "User should be logged in");
+    }
 
 }
