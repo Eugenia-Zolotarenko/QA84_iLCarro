c9f73a0a8df15a1938e9ea7da4d9132e6cb9fbc6
Arkadii <qatiktak@gmail.com>
2026-09-21 09:03:35 +0200

    Add2 isPageTitleSignUpCorrectPositiveSmokeTest

--- src/main/java/app/netlify/icarro/pages/SignUpPage.java
+++ src/main/java/app/netlify/icarro/pages/SignUpPage.java
@@ -1,10 +1,17 @@
 package app.netlify.icarro.pages;
 
 import app.netlify.icarro.core.BasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SignUpPage extends BasePage {
     public SignUpPage(WebDriver driver) {
         super(driver);
+    }
+    public SignUpPage getSignUpPage() {
+        WebElement link = driver.findElement(By.cssSelector("h1[class='title']"));
+        clickWithJS(link);
+        return new SignUpPage(driver);
     }
 }
--- src/test/java/app/netlify/icarro/tests/SignUpPageTests.java
+++ src/test/java/app/netlify/icarro/tests/SignUpPageTests.java
@@ -1,7 +1,35 @@
 package app.netlify.icarro.tests;
 
 import app.netlify.icarro.core.TestBase;
+import app.netlify.icarro.pages.SignUpPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class SignUpPageTests extends TestBase {
+    SignUpPage signUp;
 
+    @BeforeMethod
+    public void preconditions() {
+        driver.navigate().to("https://icarro-v1.netlify.app/search?page=0&size=10");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement signUpLink = wait.until(
+                ExpectedConditions.elementToBeClickable(
+                        By.cssSelector("a.navigation-link[href='/register']")));
+        signUpLink.click();
+
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.tagName("h1"))
+        );
+        signUp = new SignUpPage(driver).getSignUpPage();
+    }
+    @Test(priority = 1)
+    public void isPageTitleSignUpCorrectPositiveSmokeTest() {
+        signUp.isPageTitleCorrect("Registration");
+    }
 }
