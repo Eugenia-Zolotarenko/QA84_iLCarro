dd650782c9e34c8792e975e15683ac7b890f1265
Eugenia-Zolotarenko <eu.zolotarenko@gmail.com>
2026-09-21 17:18:09 +0200

    updated type method in BasePage

--- src/test/java/app/netlify/icarro/tests/LoginTests.java
+++ src/test/java/app/netlify/icarro/tests/LoginTests.java
@@ -1,19 +1,21 @@
 package app.netlify.icarro.tests;
 
 import app.netlify.icarro.core.TestBase;
+import app.netlify.icarro.pages.HomePage;
 import app.netlify.icarro.pages.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Method;
+
 public class LoginTests extends TestBase {
 
     LoginPage login;
-
-    @BeforeMethod
-    public void precondition() {
-        driver.get("https://icarro-v1.netlify.app/login");
-        login = new LoginPage(driver);
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(Method method, Object[] p) {
+        super.setUp(method, p);
+        login = new HomePage(driver).getLoginPage();
     }
 
     @Test
