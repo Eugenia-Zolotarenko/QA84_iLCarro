6f997e82d6547083d7a66bb3fdf53f8918397046
dimak <dimakletkin6@gmail.com>
2026-09-21 09:56:17 +0200

    Add required car field validation and recovery tests

--- src/test/java/app/netlify/icarro/tests/LetCarWorkTests.java
+++ src/test/java/app/netlify/icarro/tests/LetCarWorkTests.java
@@ -4,17 +4,46 @@ import app.netlify.icarro.core.TestBase;
 import app.netlify.icarro.pages.HomePage;
 import app.netlify.icarro.pages.LetCarWorkPage;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class LetCarWorkTests extends TestBase {
     LetCarWorkPage car;
+
     @BeforeMethod
-    public void preconditions(){
+    public void preconditions() {
+        driver = app.getDriver();
         car = new HomePage(driver).getLetCarWorkPage();
+        driver.navigate().refresh();
     }
 
     @Test
-    public void isPageTitleCorrectPositiveSmokeTest(){
+    public void isPageTitleCorrectPositiveSmokeTest() {
         car.isPageTitleCorrect("Let the car work");
+    }
+
+    @DataProvider(name = "requiredFields")
+    public Object[][] requiredFields() {
+        return new Object[][] {
+                {"manufacture", "Toyota", "Make"},
+                {"model", "Corolla", "Model"},
+                {"year", "2020", "Year"},
+                {"fuel", "petrol", "Fuel"},
+                {"seats", "5", "Seats"},
+                {"carClass", "Economy", "Car class"},
+                {"serialNumber", "QA12345", "Car registration number"},
+                {"pricePerDay", "50", "Price"}
+        };
+    }
+
+    @Test(dataProvider = "requiredFields")
+    public void requiredFieldBlocksSubmitAndRecovers(String field, String value, String label) {
+        car.fillValidForm().verifySubmitEnabled(true);
+        car.fillField(field, "")
+                .verifyRequired(field, true)
+                .verifySubmitEnabled(false);
+        car.fillField(field, value)
+                .verifyRequired(field, false)
+                .verifySubmitEnabled(true);
     }
 }
